5282f5a1c3e301ae1de72b31003e918ca090bc66
Gokul07013 <gokul07012016@gmail.com>
2024-06-19 14:59:35 +0530

    icon

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base : "/Contact-Form/"
+   base : "/Contact-Form/",
+   build: {
+    rollupOptions: {
+      external: ['react-icons/md']
+    }
+    
+  } 
 
 })
